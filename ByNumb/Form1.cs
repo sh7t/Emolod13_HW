b30b7c516cc6c4e360bcb05a55a21ea8a730d99d
sh7t <karimgaldonsky@gmail.com>
2025-09-21 12:42:54 +0300

    Unnecessary libraries removed

--- ByNumb/Form1.cs
+++ ByNumb/Form1.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +16,7 @@ namespace ByNumb
 
         class Player
         {
-            // Variables
+            // Fields
             private string name = null;
             private int level = 0;
             private int experience = 0, experienceForLevelUp = 0;
@@ -35,7 +32,8 @@ namespace ByNumb
             private Armor armor = null;
 
             // Init
-            public Player(string name, int level, int experienceForLevelUp, int strength, int endurance, int agility, int intelligence)
+            public Player(string name, int level, int experienceForLevelUp, int strength, 
+                int endurance, int agility, int intelligence)
             {
                 this.name = name;
                 this.level = level;
@@ -114,10 +112,12 @@ namespace ByNumb
         }
         class Weapon
         {
+            // Fields
             private string name = null;
             private double price = 0;
             private int attackBonus = 0;
 
+            // Init
             public Weapon(string name, double price, int attackBonus)
             {
                 this.name = name;
@@ -125,6 +125,7 @@ namespace ByNumb
                 this.attackBonus = attackBonus;
             }
 
+            // Get-set'ters
             public string getName() { return name; }
             public void setName(string name) { this.name = name;  }
 
@@ -136,10 +137,12 @@ namespace ByNumb
         }
         class Armor
         {
+            // Fields
             private string name = null;
             private double price = 0;
             private int defenseBonus = 0;
 
+            // Init
             public Armor(string name, double price, int defenseBonus)
             {
                 this.name = name;
@@ -147,6 +150,7 @@ namespace ByNumb
                 this.defenseBonus = defenseBonus;
             }
 
+            // Get-set'ters
             public string getName() { return name; }
             public void setName(string name) { this.name = name; }
 
@@ -158,14 +162,74 @@ namespace ByNumb
         }
         class Enemy
         {
+            // Fields
+            private string name = null;
+            private int level = 0;
+            private int attack = 0;
+            private int defense = 0;
+            private int healthPoints = 0;
+            private int maxHealthPoints = 0;
+            private int goldReward = 0;
+            private int experienceReward = 0;
 
+            // Init
+            public Enemy(string name, int level, int attack, int defense, 
+                int healthPoints, int maxHealthPoints, int goldReward, int experienceReward, Player opponent)
+            {
+                this.name = name;
+                this.level = (opponent.getLevel() + 1);
+                this.attack = attack;
+                this.defense = defense;
+                this.healthPoints = healthPoints;
+                this.maxHealthPoints = maxHealthPoints;
+                this.goldReward = goldReward;
+                this.experienceReward = experienceReward;
+            }
+
+            // Get-set'ters
+            public string getName() { return name; }
+            public void setName(string name) { this.name = name; }
+
+            public int getLevel() { return level; }
+            public void setLevel(int level) { this.level = level; }
+
+            public int getAttack() { return attack; }
+            public void setAttack(int attack) { this.attack = attack; }
+
+            public int getDefense() { return defense; }
+            public void setDefense(int defense) { this.defense = defense; }
+
+            public int getHealthPoints() { return healthPoints; }
+            public void setHealthPoints(int healthPoints) { this.healthPoints = healthPoints; }
+
+            public int getMaxHealthPoints() {return maxHealthPoints;}
+            public void setMaxHealthPoints(int maxHealthPoints) { this.maxHealthPoints = maxHealthPoints; }
+
+            public int getGoldReward() { return goldReward; }
+            public void setGoldReward(int goldReward) { this.goldReward = goldReward; }
+
+            public int getExperienceReward() { return experienceReward; }
+            public void setExperienceReward(int experienceReward) { this.experienceReward = experienceReward; }
         }
         class Event
         {
+            // Fields
 
+            // Init
+
+            // Get-set'ters
+
+            // Methods
         }
         class Shop
         {
+            // Fields
+
+            // Init
+
+            // Get-set'ters
+
+            // Methods
             public void GenerateItems()
             {
 
@@ -177,7 +241,13 @@ namespace ByNumb
         }
         class GameEngine
         {
+            // Fields
 
+            // Init
+
+            // Get-set'ters
+
+            // Methods
         }
     }
 }
