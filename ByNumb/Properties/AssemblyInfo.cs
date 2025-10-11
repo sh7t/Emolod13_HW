093acc6d6cfd862d29b9efb1e00379122b426b89
sh7t <karimgaldonsky@gmail.com>
2025-10-12 01:00:27 +0300

    New version commit

--- ByNumb/Properties/AssemblyInfo.cs
+++ ByNumb/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ByNumb")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Reign of Pure Glory")]
+[assembly: AssemblyDescription("Reign of Pure Glory or RPG - first game by Numb")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ByNumb")]
+[assembly: AssemblyCompany("ByNumb")]
+[assembly: AssemblyProduct("RPG")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
